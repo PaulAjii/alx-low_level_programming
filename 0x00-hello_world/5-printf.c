e24cacf789550bc0adbd2187165e860e0ce67c41
PaulAjii <paulajii134@gmail.com>
2023-06-15 19:47:38 -0700

    Add: 4-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * Description: main - prints out a string to stdout.
+ * Return: return 0 if success.
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art");
+	return (0);
+}
