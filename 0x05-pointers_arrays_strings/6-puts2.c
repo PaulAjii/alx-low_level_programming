b7c190446dda4a1d52677b4760efe4de754582b9
PaulAjii <paulajii134@gmail.com>
2023-06-26 21:42:22 -0700

    6-puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,11 +8,14 @@
 
 void puts2(char *str)
 {
-	int length;
+	int length = 0;
 
-	for (length = 0; str[length] != '\0'; length = length + 2)
+	while (str[length] != '\0')
 	{
-		_putchar(str[length]);
+		if (length % 2 == 0)
+		{
+			_putchar(str[length]);
+		}
 	}
 	_putchar('\n');
 }
